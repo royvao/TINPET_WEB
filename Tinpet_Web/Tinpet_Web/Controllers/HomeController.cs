17a6532d4a46075b5595c83fad23d3a109eacfb9
royvao <royvao@gmail.com>
2023-05-22 17:05:41 +0200

    add proyect page, update interface

--- Tinpet_Web/Tinpet_Web/Controllers/HomeController.cs
+++ Tinpet_Web/Tinpet_Web/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace Tinpet_Web.Controllers
         {
             return View();
         }
+        public IActionResult Proyect()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
