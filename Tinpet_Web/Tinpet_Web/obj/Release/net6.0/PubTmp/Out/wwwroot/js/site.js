7f9f9535ebcc3773f86d4d424c8f8af95e8f708c
royvao <royvao@gmail.com>
2023-06-05 15:44:19 +0200

    update interface, all pages reformated

--- Tinpet_Web/Tinpet_Web/obj/Release/net6.0/PubTmp/Out/wwwroot/js/site.js
+++ Tinpet_Web/Tinpet_Web/obj/Release/net6.0/PubTmp/Out/wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+// Función para actualizar la visibilidad de los carruseles
+function updateCarouselVisibility() {
+	if ($(window).width() >= 600) {
+		// Pantalla grande: mostrar el carrusel grande y ocultar el carrusel pequeño
+		$('#carouselLarge').show();
+		$('#carouselSmall').hide();
+	} else {
+		// Pantalla pequeña: mostrar el carrusel pequeño y ocultar el carrusel grande
+		$('#carouselSmall').show();
+		$('#carouselLarge').hide();
+	}
+}
+
+// Actualizar la visibilidad de los carruseles al cargar la página
+$(document).ready(updateCarouselVisibility);
+
+// Actualizar la visibilidad de los carruseles cuando se cambia el tamaño de la ventana
+$(window).resize(updateCarouselVisibility);
+
